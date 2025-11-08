9dc70dc7738163fefa598f35119e94d051449e43
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-08 15:31:51 +0700

    Menyelesaikan codingan dan fox semuaa error

--- app/src/main/java/com/example/questnavigasitugas_159/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigasitugas_159/view/Beranda.kt
@@ -27,18 +27,18 @@ fun Beranda(onMasuk: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Selamat Datang", modifier = Modifier.padding(bottom = 16.dp))
-            // ganti drawable/ic_logo dengan logo kamu di res/drawable
+            Text(text = "Selamat Datang", modifier = Modifier.padding(bottom = 50.dp))
+
             Image(
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = "Logo",
-                modifier = Modifier.size(220.dp)
+                modifier = Modifier.size(350.dp)
             )
             Spacer(modifier = Modifier.height(24.dp))
             Text(text = "Muhammad Dzulfikri\n20230140159")
             Spacer(modifier = Modifier.height(32.dp))
             Button(onClick = onMasuk) {
-                Text(text = stringResource(id = R.string.submit)) // gunakan string submit sebagai "Masuk"
+                Text(text = stringResource(id = R.string.submit))
             }
         }
     }
