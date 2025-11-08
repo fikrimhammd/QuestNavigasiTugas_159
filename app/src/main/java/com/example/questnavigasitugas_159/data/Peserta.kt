c224f19c2215516e36336dd97f238607cf560abd
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-08 13:07:48 +0700

    membuat data class Peserta di Peserta.kt

--- app/src/main/java/com/example/questnavigasitugas_159/data/Peserta.kt
+++ app/src/main/java/com/example/questnavigasitugas_159/data/Peserta.kt
@@ -1,2 +1,8 @@
 package com.example.questnavigasitugas_159.data
 
+data class Peserta(
+    val nama: String,
+    val jenisKelamin: String,
+    val statusPerkawinan: String,
+    val alamat: String
+)
