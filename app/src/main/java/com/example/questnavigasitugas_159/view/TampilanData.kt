62f7430a0c93da73c1e50d00e31495057bc794a0
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-08 15:23:59 +0700

    Membuat Membuat TampilanData.kt

--- app/src/main/java/com/example/questnavigasitugas_159/view/TampilanData.kt
+++ app/src/main/java/com/example/questnavigasitugas_159/view/TampilanData.kt
@@ -1,2 +1,62 @@
-package com.example.questnavigasitugas_159.view
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.ExperimentalMaterial3Api
 
+import com.example.questnavigasitugas_159.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(onBackBtnClick:()->Unit){
+    // contoh statis
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+    )
+
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {Text(text = stringResource(id = R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700))
+            )
+        }){ isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween){
+            Column(modifier = Modifier.padding(all = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(space = 10.dp)){
+                items.forEach{ item ->
+                    Column{
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontSize = 22.sp)
+                        HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                        Spacer(modifier = Modifier.height(height = 10.dp))
+                    }
+                }
+            }
+            Button(modifier = Modifier.fillMaxWidth(),
+                onClick = onBackBtnClick){
+                Text(text = stringResource(id = R.string.back))
+            }
+        }
+    }
+}
