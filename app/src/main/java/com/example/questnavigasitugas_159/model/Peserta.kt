9a150ee5fe1a76e3bbbfcbd860ece6ab3ce0bcc6
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-08 15:23:28 +0700

    Membuat Peserta.kt

---
+++ app/src/main/java/com/example/questnavigasitugas_159/model/Peserta.kt
@@ -0,0 +1,8 @@
+package com.example.questnavigasitugas_159.model
+
+data class Peserta(
+    val nama: String,
+    val jenisKelamin: String,
+    val statusPerkawinan: String,
+    val alamat: String
+)
